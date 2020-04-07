1e121282fd7c4dbbadc22c03cc8265b9a9cc9a3b
jovialcore <jollydosky@gmail.com>
2020-04-07 08:24:31 -0700

    added some helpfull comments to the app

--- app/Http/Controllers/controlCart.php
+++ app/Http/Controllers/controlCart.php
@@ -36,26 +36,33 @@ class controlCart extends Controller
 
 //save for later cart
     public function saveForLater($id) {
+    	//this feature took me real good 5 days to fix. Mehn!!!
+    	//it is a validation that prevents a cart item from being added to saveForLater when it already exist!
+
+    	//here is how it works
     	 $idSave = \Cart::instance('default')->get($id);
-  
+  // search in the default intance class cart first for the cart item id that is being sent across from the serve
     	$dup = \Cart::instance('default')->search(function($cartItem, $rowId) use ($id){	
     				return $rowId === $id;
 
     			});
     		if ($dup->isNotEmpty()){
+    			//if it is empty, the search for the id in the saveForLater cart for that id.
 
     			$dupl = \Cart::instance('saveForLater')->search(function($cartItem, $rowId) use ($id){
     					
     				return $rowId === $id;
 
     			});	
-    			
+    			//if Id of the cart item is inside the two carts...
     		if ($dup->isNotEmpty() and $dupl->isNotEmpty()) {
 
+    			//then say that it already exist in the cart...
     		return 	redirect('/cart')->with("success", "{$idSave->name} already exist in the cart ");
     
 }     			 
-	else {	
+	else {	//if not, then save the item to savForLater cart and redirect with the message
+
 		$idSave = \Cart::instance('default')->get($id);
 		\Cart::remove($id); 
 		\Cart::instance('saveForLater')->add($idSave->id, $idSave->name,1,3)->associate('App\ecomm');	
