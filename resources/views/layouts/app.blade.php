001b2b161bdff95b5cd1387c67bd9995943ddfb8
jovialcore <jollydosky@gmail.com>
2020-04-25 08:02:35 -0700

    made auth changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,14 +24,14 @@
     <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-      <a class="navbar-brand" href="/home">Covid-19 Store</a>
+      <a class="navbar-brand" href="/">Covid-19 Store</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="/home">Home
+            <a class="nav-link" href="/">Home
          <!--      <span class="sr-only">(current)</span> no useful -->
             </a>
           </li>
